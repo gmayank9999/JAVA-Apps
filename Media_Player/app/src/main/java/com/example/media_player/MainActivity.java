25094ceb7a4e345e0a284003234a0159b8ffa909
Mayank Gupta <gmayank9999@gmail.com>
2025-04-15 22:48:33 +0530

    Media Player app

---
+++ Media_Player/app/src/main/java/com/example/media_player/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.media_player;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.videoView);
+
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.sample;
+        Uri uri = Uri.parse(videoPath);
+
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
+    }
+}
